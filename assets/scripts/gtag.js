fc9bcb41a27cc04fc6344d461ccd4cc6fe476b32
drrajarameshd <95187505+drrajarameshd@users.noreply.github.com>
2025-09-08 23:03:42 +0530

    Update gtag.js

--- assets/scripts/gtag.js
+++ assets/scripts/gtag.js
@@ -1,9 +1,9 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17523822825"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9FBX0G12S5"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'AW-17523822825');
+  gtag('config', 'G-9FBX0G12S5');
 </script>
